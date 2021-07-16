65839390090bdc2aeaa6aa6ab0babf118ee36cbb
João Pedro Giandoso <jpgiandoso@gmail.com>
2021-07-16 12:56:06 -0300

    v1, sem responsividade, sem página de trabalhos

---
+++ js/venta-bg.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+    el: "#bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x14141b,
+    shininess: 54.00,
+    waveHeight: 2.50,
+    waveSpeed: 1.40,
+    zoom: 0.75
+  })
